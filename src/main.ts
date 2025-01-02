9fc5854eefec8a36e9331f41ac2130a25a4b1f2c
nnamdi nwaocha <nnamdinwaocha@gmail.com>
2025-01-02 19:28:41 +0100

    again....

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,16 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as express from 'express';
 
-const expressApp = express();
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-
-  app.enableCors();
-  app.setGlobalPrefix('api'); // Optional prefix
-
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors(); // Enable CORS if needed
+  app.setGlobalPrefix('api'); // Prefix all routes with '/api'
   await app.init();
 }
 
 bootstrap();
 
-// Export serverless function handler for Vercel
-export const handler = (req: any, res: any) => {
-  expressApp(req, res);
-};
+// Export handler for Vercel
+export const handler = server;
